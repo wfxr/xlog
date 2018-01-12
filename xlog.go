2e3a9609b3708ab0927149be319ef8d0b7ebe02e
Wenxuan <wenxuangm@gmail.com>
2018-01-12 15:19:45 +0800

    Rename refactor

--- xlog.go
+++ xlog.go
@@ -17,54 +17,54 @@ func init() {
 	zerolog.TimeFieldFormat = "2006-01-02 15:04:05.000"
 }
 
-func logWithLevel(event *zerolog.Event, msg ...interface{}) {
+func withLevel(event *zerolog.Event, msg ...interface{}) {
 	event.Msg(fmt.Sprint(msg...))
 }
 
-func logfWithLevel(event *zerolog.Event, format string, v ...interface{}) {
+func withLevelf(event *zerolog.Event, format string, v ...interface{}) {
 	event.Msgf(format, v...)
 }
 
-func logWhenErrWithLevel(event *zerolog.Event, err error, msg ...interface{}) {
+func withLevelWhenError(event *zerolog.Event, err error, msg ...interface{}) {
 	if err != nil {
 		event.Err(err).Msg(fmt.Sprint(msg...))
 	}
 }
 
-func logfWhenErrWithLevel(event *zerolog.Event, err error, format string, v ...interface{}) {
+func withLevelWhenErrorf(event *zerolog.Event, err error, format string, v ...interface{}) {
 	if err != nil {
 		event.Err(err).Msgf(format, v)
 	}
 }
 
-func LogDebug(msg ...interface{}) {
-	logWithLevel(log.Debug(), msg...)
+func Debug(msg ...interface{}) {
+	withLevel(log.Debug(), msg...)
 }
 
-func LogInfo(msg ...interface{}) {
-	logWithLevel(log.Info(), msg...)
+func Info(msg ...interface{}) {
+	withLevel(log.Info(), msg...)
 }
 
-func LogWarn(err error, msg ...interface{}) {
-	logWhenErrWithLevel(log.Warn(), err, msg...)
+func Warn(err error, msg ...interface{}) {
+	withLevelWhenError(log.Warn(), err, msg...)
 }
 
-func LogFatal(err error, msg ...interface{}) {
-	logWhenErrWithLevel(log.Fatal(), err, msg...)
+func Fatal(err error, msg ...interface{}) {
+	withLevelWhenError(log.Fatal(), err, msg...)
 }
 
-func LogfDebug(format string, v ...interface{}) {
-	logfWithLevel(log.Debug(), format, v...)
+func Debugf(format string, v ...interface{}) {
+	withLevelf(log.Debug(), format, v...)
 }
 
-func LogfInfo(format string, v ...interface{}) {
-	logfWithLevel(log.Info(), format, v...)
+func Infof(format string, v ...interface{}) {
+	withLevelf(log.Info(), format, v...)
 }
 
-func LogfFatal(err error, format string, v ...interface{}) {
-	logfWhenErrWithLevel(log.Fatal(), err, format, v...)
+func Fatalf(err error, format string, v ...interface{}) {
+	withLevelWhenErrorf(log.Fatal(), err, format, v...)
 }
 
-func LogfWarn(err error, format string, v ...interface{}) {
-	logfWhenErrWithLevel(log.Warn(), err, format, v...)
+func Warnf(err error, format string, v ...interface{}) {
+	withLevelWhenErrorf(log.Warn(), err, format, v...)
 }
